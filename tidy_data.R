344ca8193344b49bf4e8fc6478a79e62ddaddff2
raybuhr <rebuhr@gmail.com>
2017-05-08 23:09:33 -0500

    adding working to create interactive chart

---
+++ tidy_data.R
@@ -0,0 +1,20 @@
+library(readxl)
+library(dplyr)
+library(tidyr)
+
+combine_timms_data <- function(question) {
+  q <- read_xlsx(path = "T15_G8_MAT_ItemPercentCorrect.xlsx",
+                 sheet = question,
+                 range = "C6:D45",
+                 col_types = c("text", "numeric"),
+                 col_names = c("country", paste("question", question, sep = "_")))
+}
+
+timms_data <- combine_timms_data(1)
+
+for(i in 2:215) {
+  timms_data <- timms_data %>%
+    left_join(combine_timms_data(i), by = "country")
+}
+
+timms_data <- gather(data = timms_data, key = "question", value = "percent_correct", -country)
